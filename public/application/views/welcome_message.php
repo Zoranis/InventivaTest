b7e1b72a50511b4cf9b1db727921d61b234c5a5f
ran <ran@example.com>
2017-03-22 03:00:25 +0200

    added inline required to input

--- public/application/views/welcome_message.php
+++ public/application/views/welcome_message.php
@@ -28,21 +28,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="form-group row">
 				<label for="text-input" class="col-2 col-form-label">User Name</label>
 				<div class="col-7">
-					<input class="form-control" name="userName" type="text" value="" id="userName">
+					<input class="form-control" name="userName" type="text" value="" id="userName" required>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="password-input" class="col-2 col-form-label">Password</label>
 				<div class="col-7">
-					<input class="form-control" name="password" type="password" value="" id="password">
+					<input class="form-control" name="password" type="password" value="" id="password" required>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="email-input" class="col-2 col-form-label">Email</label>
 				<div class="col-7">
-					<input class="form-control" name="email" type="email" value="" id="email">
+					<input class="form-control" name="email" type="email" value="" id="email" required>
 				</div>
 			</div>
 
@@ -64,17 +64,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="form-group row">
 				<label for="tel-input" class="col-2 col-form-label">Phone</label>
 				<div class="col-2">
-					<input class="form-control" name="phonePrefix" type="tel" value="" id="phonePrefix">
+					<input class="form-control" name="phonePrefix" type="tel" value="" id="phonePrefix" required>
 				</div>
 				-
 				<div class="col-5">
-					<input class="form-control" name="phoneSuffix" type="tel" value="" id="phoneSuffix">
+					<input class="form-control" name="phoneSuffix" type="tel" value="" id="phoneSuffix" required>
 				</div>
 			</div>
 
 			<div class="form-check">
 				<label class="form-check-label">
-					<input type="checkbox" name="termsApproved" class="form-check-input" id="termsApproved">
+					<input type="checkbox" name="termsApproved" class="form-check-input" id="termsApproved" required>
 					Terms & Conditions
 				</label>
 			</div>
@@ -93,6 +93,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="crossorigin="anonymous"></script>
 	<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.js" charset="utf-8"></script>
 	<script src="/js/accounts.js" charset="utf-8"></script>
 
 </body>
